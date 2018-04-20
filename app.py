9cfe548b95489d53a630d8bcd823df092b1894e7
Maria Javier <mariabjavier@gmail.com>
2018-04-20 15:46:32 -0400

    able to render html with query results

--- app.py
+++ app.py
@@ -10,14 +10,17 @@ def main():
 
 @app.route('/pokemon/<query>', methods=['GET'])
 def poke(query):
-	url = 'http://pokeapi.co/api/v2/pokemon/' + query
+	
+	url = 'http://pokeapi.co/api/v2/pokemon/' + query.lower()
 	r = requests.get(url)
 
-	poke_data = r.json()
-	print(poke_data["id"])
-	print(poke_data["forms"][0]['name'])
-	
-	return render_template('pokemon.html')
+	if r.status_code != 200:
+		return '<html><body><h1>Sorry No Pokemon Found!</h1></body></html>'
+	else:
+		poke_data = r.json()
+		poke_id = poke_data["id"]
+		poke_name = poke_data["forms"][0]['name']
+		return render_template('pokemon.html', query=query, name=poke_name, value=poke_id)
 
 if __name__ == '__main__':
     app.run()
